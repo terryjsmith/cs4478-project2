9d303b772978321576fb60111e034f8a2c9e2ddc
Terry Smith <terry@icedteapowered.com>
2023-04-03 20:18:59 -0400

    add flashing state, hit points to zombie

--- Assets/Scripts/Zombie.cs
+++ Assets/Scripts/Zombie.cs
@@ -4,6 +4,14 @@ using UnityEngine;
 
 public class Zombie : MonoBehaviour
 {
+    [SerializeField]
+    int health;
+
+    [SerializeField]
+    float flashingTimer;
+
+    float m_countdownTimer;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,17 +21,34 @@ public class Zombie : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        // If we're flashing, flash for a while then stop
+        if(m_countdownTimer > 0)
+        {
+            m_countdownTimer -= Time.deltaTime;
+            if(m_countdownTimer <= 0)
+            {
+                GetComponent<Animator>().SetBool("ZombieFlashing", false);
+            }
+        }
     }
 
     void OnTriggerEnter2D(Collider2D collision)
     {
         if(collision.gameObject.tag == "Bullet")
         {
-            Debug.Log("Zombie dying");
-            GetComponent<Animator>().SetBool("ZombieDying", true);
+            health--;
+            if (health > 0)
+            {
+                GetComponent<Animator>().SetBool("ZombieFlashing", true);
+                m_countdownTimer = flashingTimer;
+            }
+            else
+            {
+                GetComponent<Animator>().SetBool("ZombieDying", true);
+                GameObject.Destroy(gameObject, 1.25f);
+            }
+            
             GameObject.Destroy(collision.gameObject);
-            GameObject.Destroy(gameObject, 1.25f);
         }
     }
 }
