368bcb244ed4a2e5bf39016cc4a4078b07827350
Terry Smith <terry@icedteapowered.com>
2023-04-04 10:40:30 -0400

    make zombies attack player

--- Assets/Scripts/Zombie.cs
+++ Assets/Scripts/Zombie.cs
@@ -16,15 +16,14 @@ public class Zombie : MonoBehaviour
     [SerializeField]
     private float walkRadius = 5.0f;
 
-    [SerializeField]
-    private float attackInterval = 1.0f;
-
     private Rigidbody2D m_rigidBody;
     private Animator m_animator;
     private SpriteRenderer m_spriteRenderer;
     private float startX;
 
     float m_countdownTimer;
+
+    GameObject player;
 
     // Start is called before the first frame update
     void Start()
@@ -41,7 +40,7 @@ public class Zombie : MonoBehaviour
             m_animator.SetBool("ZombieWalking", true);
         }
 
-        StartCoroutine(PerformAttack());
+        player = GameObject.Find("Character");
     }
 
     // Update is called once per frame
@@ -58,13 +57,16 @@ public class Zombie : MonoBehaviour
         }
 
         // Move the zombie
-        if (m_rigidBody && !m_animator.GetBool("ZombieAttacking")) // Only move if the zombie is not attacking
+        if (m_rigidBody)
         {
-            m_rigidBody.velocity = new Vector2(zombieSpeed, m_rigidBody.velocity.y);
-        }
-        else
-        {
-            m_rigidBody.velocity = new Vector2(0.0f, m_rigidBody.velocity.y);
+            if (!m_animator.GetBool("ZombieAttacking")) // Only move if the zombie is not attacking
+            {
+                m_rigidBody.velocity = new Vector2(zombieSpeed, m_rigidBody.velocity.y);
+            }
+            else
+            {
+                m_rigidBody.velocity = new Vector2(0.0f, m_rigidBody.velocity.y);
+            }
         }
 
         // startX + walkRadius or startX - walkRadius boundaries
@@ -72,36 +74,47 @@ public class Zombie : MonoBehaviour
         {
             // Change direction to the left
             zombieSpeed = -Mathf.Abs(zombieSpeed);
-            m_spriteRenderer.flipX = true;
         }
         else if (transform.position.x <= startX - walkRadius)
         {
             // Change direction to the right
             zombieSpeed = Mathf.Abs(zombieSpeed);
+        }
+
+        // Reset flip state each frame
+        if(zombieSpeed < 0.0f)
+        {
+            m_spriteRenderer.flipX = true;
+        }
+        else
+        {
             m_spriteRenderer.flipX = false;
         }
-    }
 
-    IEnumerator PerformAttack()
-    {
-        while (true)
+        // Measure distance from player
+        float distance = Vector3.Distance(player.transform.position, transform.position);
+        if(distance < 4.0f)
         {
-            // Stop moving and play the attack animation
             m_animator.SetBool("ZombieAttacking", true);
-            yield return new WaitForSeconds(1); 
 
-            // Resume walking and wait for 5 seconds before attacking again
+            // Make sure we're facing the player to attack them
+            if (player.transform.position.x < transform.position.x)
+            {
+                m_spriteRenderer.flipX = true;
+            }
+            else
+            {
+                m_spriteRenderer.flipX = false;
+            }
+        }
+        else
+        {
             m_animator.SetBool("ZombieAttacking", false);
-            yield return new WaitForSeconds(5 - 1); 
         }
     }
-
-
     
     void OnTriggerEnter2D(Collider2D collision)
     {
-
-
         if (collision.gameObject.tag == "Bullet")
         {
             health--;
@@ -113,7 +126,7 @@ public class Zombie : MonoBehaviour
             else
             {
                 GetComponent<Animator>().SetBool("ZombieDying", true);
-                GameObject.Destroy(gameObject, 1.25f);
+                GameObject.Destroy(gameObject, 1.0f);
 
                 // Remove the rigidbody and collider so the player can step over a dead body without losing health
                 Destroy(GetComponent<Rigidbody2D>());
@@ -124,150 +137,3 @@ public class Zombie : MonoBehaviour
         }
     }
 }
-
-
-/*
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class Zombie : MonoBehaviour
-{
-    [SerializeField]
-    int health;
-
-    [SerializeField]
-    float flashingTimer;
-
-    [SerializeField]
-    private float zombieSpeed = 1.0f;
-
-    [SerializeField]
-    private float walkRadius = 5.0f;
-
-    [SerializeField]
-    private float attackInterval = 1.0f;
-
-    private Rigidbody2D m_rigidBody;
-    private Animator m_animator;
-    private SpriteRenderer m_spriteRenderer;
-    private float startX;
-
-    float m_countdownTimer;
-    private bool playerInRange = false;
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        m_rigidBody = GetComponent<Rigidbody2D>();
-        m_animator = GetComponent<Animator>();
-        m_spriteRenderer = GetComponent<SpriteRenderer>();
-
-        startX = transform.position.x;
-
-        // walking animation
-        if (m_animator != null)
-        {
-            m_animator.SetBool("ZombieWalking", true);
-        }
-
-        StartCoroutine(PerformAttack());
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        // If we're flashing, flash for a while then stop
-        if (m_countdownTimer > 0)
-        {
-            m_countdownTimer -= Time.deltaTime;
-            if (m_countdownTimer <= 0)
-            {
-                GetComponent<Animator>().SetBool("ZombieFlashing", false);
-            }
-        }
-
-        // Move the zombie
-        if (m_rigidBody && !m_animator.GetBool("ZombieAttacking")) // Only move if the zombie is not attacking
-        {
-            m_rigidBody.velocity = new Vector2(zombieSpeed, m_rigidBody.velocity.y);
-        }
-        else
-        {
-            m_rigidBody.velocity = new Vector2(0.0f, m_rigidBody.velocity.y);
-        }
-
-        // startX + walkRadius or startX - walkRadius boundaries
-        if (transform.position.x >= startX + walkRadius)
-        {
-            // Change direction to the left
-            zombieSpeed = -Mathf.Abs(zombieSpeed);
-            m_spriteRenderer.flipX = true;
-        }
-        else if (transform.position.x <= startX - walkRadius)
-        {
-            // Change direction to the right
-            zombieSpeed = Mathf.Abs(zombieSpeed);
-            m_spriteRenderer.flipX = false;
-        }
-    }
-
-    IEnumerator PerformAttack()
-    {
-        while (true)
-        {
-            if (playerInRange)
-            {
-                // Stop moving and play the attack animation
-                m_animator.SetBool("ZombieAttacking", true);
-                yield return new WaitForSeconds(1); // Replace 1 with the duration of your attack animation
-            }
-            else
-            {
-                m_animator.SetBool("ZombieAttacking", false);
-            }
-
-            // Resume walking and wait for 5 seconds before checking player's proximity again
-            yield return new WaitForSeconds(5 - 1); // Replace 1 with the duration of your attack animation and 5 with the interval between checks
-        }
-    }
-
-    void OnTriggerEnter2D(Collider2D collision)
-    {
-        if (collision.gameObject.tag == "Player")
-        {
-            playerInRange = true;
-        }
-
-        if (collision.gameObject.tag == "Bullet")
-        {
-            health--;
-            if (health > 0)
-            {
-                GetComponent<Animator>().SetBool("ZombieFlashing", true);
-                m_countdownTimer = flashingTimer;
-            }
-            else
-            {
-                GetComponent<Animator>().SetBool("ZombieDying", true);
-                GameObject.Destroy(gameObject, 1.25f);
-
-                // Remove the rigidbody and collider so the player can step over a dead body without losing health
-
-                Destroy(GetComponent<Rigidbody2D>());
-                Destroy(GetComponent<BoxCollider2D>());
-            }
-            GameObject.Destroy(collision.gameObject);
-        }
-    }
-
-    void OnTriggerExit2D(Collider2D collision)
-    {
-        if (collision.gameObject.tag == "Player")
-        {
-            playerInRange = false;
-        }
-    }
-}
-
-*/ 
