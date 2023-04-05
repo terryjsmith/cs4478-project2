4ba6acd015075d53634edbcad6afe7372b4d3890
Terry Smith <terry@icedteapowered.com>
2023-04-05 11:48:59 -0400

    tweaks to level 5

--- Assets/Scripts/ItemCollector.cs
+++ Assets/Scripts/ItemCollector.cs
@@ -10,10 +10,21 @@ public class ItemCollector : MonoBehaviour
 
    [SerializeField] private AudioSource coinSoundEffect;
 
-   private void OnTriggerEnter2D(Collider2D collision)
+    List<string> collectedCoins;
+
+    public void Start()
+    {
+        collectedCoins = new List<string>();
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
    {
         if (collision.gameObject.CompareTag("Gold"))
         {
+            // Skip processing of already collected coins due to multiple rigid bodies
+            if (collectedCoins.Contains(collision.gameObject.name)) return;
+
+            collectedCoins.Add(collision.gameObject.name);
             coinSoundEffect.Play();
             Destroy(collision.gameObject);
             Gold++;
